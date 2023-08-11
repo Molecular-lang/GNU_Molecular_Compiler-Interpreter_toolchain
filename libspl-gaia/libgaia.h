b3fd833314f61978018adef070610285a254f9cb
Phrunsys <phrunsys.scpel.256.ug@gmail.com>
2023-08-10 23:34:08 -0400

    introduce hoocks for libgaia - an internal compiler library for language runtime semantics used by the prefrontal cortex routines

---
+++ libspl-gaia/libgaia.h
@@ -0,0 +1,8 @@
+// this is the global namespace for libgaia
+
+// the runtime class creates an object interface
+class runtime;
+
+namespace lib_gaia {
+	// library classes and routines go in here
+}
